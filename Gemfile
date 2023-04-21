d770fdd686bf0d21c87e0dfe6543cd40faf00ea6
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2023-04-21 01:57:06 -0400

    Add specs for GPX::Segment

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 ruby '3.2.1'
 source 'https://rubygems.org'
 
+gem 'equivalent-xml'
 gem 'nokogiri'
+gem 'nokogiri-diff'
 gem 'zeitwerk'
 
+gem 'pry'
+gem 'rspec'
 gem 'rubocop'
 gem 'rubocop-performance'
 gem 'rubocop-rspec'
-gem 'rspec'
-gem 'pry'
---
+++ spec/segment_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe GPX::Segment do
+  let(:pause_length) { 5 }
+  let(:expected) do
+    File.open("spec/files/#{file}_expected.gpx") { |f| Nokogiri::XML(f) }
+  end
+
+  subject { described_class.new("spec/files/#{file}.gpx", pause_length: pause_length) }
+
+  describe '#rewrite' do
+    context 'for a simple file' do
+      let(:file) { 'simple' }
+
+      it 'splits track points into segments' do
+        subject.rewrite
+        expect(subject.doc).to be_equivalent_to(expected)
+      end
+    end
+  end
+end
